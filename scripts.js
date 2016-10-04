b1c7d7d3dc018d59cc908c7a8fb9f2ce7ed34bce
André Samuelsson <andresamuelsson94@gmail.com>
2016-10-04 10:20:42 +0200

    Replace php with javascript api.

--- scripts.js
+++ scripts.js
@@ -1,16 +1,38 @@
-function loadpage() {
+let likers = [];
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '560134897527816',
+    xfbml      : true,
+    version    : 'v2.6'
+  });
+  checkLoginState();
+
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+
+
+
+function loadpage(likes) {
     for (var i=0; i<10; i++) {
         for (var j=0; j<likes.length; j++) {
             var li = document.createElement("li");
-            li.innerHTML = likes[j];
+            li.innerHTML = '<div class="fb-reaction fb-'+likes[j].type+'"></div>' + ' ' + likes[j].name;
             document.getElementsByTagName("ul")[0].appendChild(li);
         }
     }
 
-    setTimeout(bingo, 1000);
+    setTimeout(() => bingo(likes), 1000);
 }
 
-function bingo() {
+function bingo(likes) {
     var min = likes.length*5 - 3;
     var max = likes.length*4 - 1;
     console.log("Likes: " + likes.length + ", Min: " + (min+3) + ", Max: " + (min+max+3) + ", Total: " + (likes.length*10));
@@ -33,4 +55,67 @@ function bingo() {
         document.getElementById("popup").style.display = "block";
         document.body.style.color = "rgba(0, 0, 0, 0.2)";
     }, 11000);
+}
+
+function getParam(paramName) {
+    let paramStrings = window.location.search.substr(1).split('&')
+    let paramMap = paramStrings.reduce((acc, item) => {
+        let [k,v] = item.split('=');
+        acc[k] = v;
+        return acc;
+    }, {})
+    return paramMap[paramName];
+}
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    if (response.status === "connected") {
+      console.log('I am the login');  
+      document.getElementById('fb-login-button').style.display = 'none';
+      accessToken = response.authResponse.accessToken;
+
+      let fb_image_id = getParam('photo_id');
+      FB.api(
+          `/${fb_image_id}/reactions`,
+          function (response) {
+            console.log(response)
+            if (response && !response.error) {
+              let next = response.paging.next;
+              if (next) {
+                likers = response.data;
+                fetchRecursive(next, likers).then(bloops => {
+                  console.log('this is the recursive', bloops);
+                  loadpage(bloops);
+                });
+              } else {
+                loadpage(response.data);
+              }
+              console.log('success');
+            }
+          }
+      );
+
+    } else {
+      console.log('You are the fail');
+    }
+  });
+
+  function fetchRecursive(next, likers = []) {
+    return fetch(next, {
+                credentials: 'same-origin'
+              })
+              .then(resp => resp.json())
+              .then(data => {
+                console.log(data);
+                likers.push(...data.data);
+                next = data.paging.next;
+
+                if (next) {
+                  return fetchRecursive(next, likers);
+                } else {
+                  return likers;
+                }
+              })
+  }
+
 }
